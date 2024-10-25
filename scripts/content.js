f06b3ccfa2c0b46282daca9bdd51c8c605d7be8b
hoangtu92 <hoangtu92@live.com>
2024-10-25 11:15:23 +0700

    Swipe down to open menu instead of click. Click will now seek forward instead

--- scripts/content.js
+++ scripts/content.js
@@ -72,8 +72,64 @@ function initYtTheme (request) {
 
             let playerOverlay = htmlToElement(`<div class="player-overlay"></div>`);
             playerOverlay.addEventListener("click", function (){
-                toggleNav();
+                video.currentTime += 20;
             });
+
+
+
+            let xDown = null;
+            let yDown = null;
+
+            function getTouches(evt) {
+                return evt.touches ||             // browser API
+                    evt.originalEvent.touches; // jQuery
+            }
+
+            function handleTouchStart(evt) {
+                const firstTouch = getTouches(evt)[0];
+                xDown = firstTouch.clientX;
+                yDown = firstTouch.clientY;
+            };
+
+
+            playerOverlay.addEventListener("touchstart", handleTouchStart);
+            playerOverlay.addEventListener("touchmove", function (evt){
+                if ( ! xDown || ! yDown ) {
+                    return;
+                }
+
+                var xUp = evt.touches[0].clientX;
+                var yUp = evt.touches[0].clientY;
+
+                var xDiff = xDown - xUp;
+                var yDiff = yDown - yUp;
+
+                if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                    if ( xDiff > 0 ) {
+                        /* left swipe */
+                        console.log("Left swipe")
+                    } else {
+                        /* right swipe */
+                        console.log("Right swipe")
+
+                    }
+                } else {
+                    if ( yDiff > 0 ) {
+                        /* up swipe */
+                        console.log("Up swipe")
+
+
+                    } else {
+                        /* down swipe */
+                        console.log("Down swipe")
+                        showNav();
+                    }
+                }
+                /* reset values */
+                xDown = null;
+                yDown = null;
+            });
+
             document.querySelector("#player-container-inner").append(playerOverlay);
 
 
