45feb5a02b785decf25027018ff14eedbbc416c4
hoangtu92 <hoangtu92@live.com>
2026-04-20 22:13:27 +0700

    update

--- start.cmd
+++ start.cmd
@@ -1,7 +1,6 @@
 @ECHO OFF
 git pull
 start "" "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" ^
-  --user-data-dir="C:\chrome-kiosk" ^
   --profile-directory="Profile 2" ^
   --kiosk ^
   --no-first-run ^
