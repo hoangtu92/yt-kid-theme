debb5c1ef76c8bd63e04791b0f72464377e22b33
hoangtu92 <hoangtu92@live.com>
2026-04-20 20:12:37 +0700

    update

---
+++ start.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+git pull
+start "" "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" ^
+  --profile-directory="Profile 2" ^
+  --kiosk ^
+  --no-first-run ^
+  --disable-infobars ^
+  --disable-session-crashed-bubble ^
+  --app-id=jmdmbgfgbffefjkplbimedofoeeojcii
