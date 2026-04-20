19dfd53c86fe786c4033aad6cd7008e8f5188f7f
hoangtu92 <hoangtu92@live.com>
2026-04-20 22:11:57 +0700

    update

--- start.cmd
+++ start.cmd
@@ -2,7 +2,7 @@
 git pull
 start "" "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" ^
   --user-data-dir="C:\chrome-kiosk" ^
-  --profile-directory="Default" ^
+  --profile-directory="Profile 2" ^
   --kiosk ^
   --no-first-run ^
   --disable-infobars ^
