f72d4d170ed16008ed0dc2cbb51fc79d673d2b10
hoangtu92 <hoangtu92@live.com>
2026-04-26 23:26:48 +0700

    search

--- src/core/presets/search.js
+++ src/core/presets/search.js
@@ -5,10 +5,10 @@ export const searchPresets = {
         { type: "action", action: "switchLanguage", targetLang: "en-US", icon: "dad.gif" },
 
         { type: "search", query: "Nhạc thiếu nhi", icon: "music.gif" },
-        { type: "search", query: "Hoạt hình", icon: "video.gif" },
+        { type: "search", query: "Đọc truyện", icon: "book.gif" },
         { type: "search", query: "Học nói, học viết", icon: "abc.gif" },
         { type: "search", query: "Học vẽ, tô màu", icon: "painting.gif" },
-        { type: "search", query: "trò chơi, sáng tạo", icon: "children.gif" },
+        { type: "search", query: "vui chơi", icon: "confetti.gif" },
 
         { type: "action", action: "voiceRecognition", icon: "podcast.gif" }
     ],
@@ -17,10 +17,10 @@ export const searchPresets = {
         { type: "action", action: "switchLanguage", targetLang: "vi-VN", icon: "mom.gif" },
 
         { type: "search", query: "Kids songs", icon: "music_1.gif" },
-        { type: "search", query: "Disney, cartoon, 3D animated", icon: "video_1.gif" },
+        { type: "search", query: "kid story book", icon: "book_1.gif" },
         { type: "search", query: "learn English, learn to speak", icon: "abc_1.gif" },
         { type: "search", query: "Painting, color, drawing", icon: "painting_1.gif" },
-        { type: "search", query: "outdoor, fun game", icon: "children_1.gif" },
+        { type: "search", query: "caitie's classrom", icon: "confetti_1.gif" },
 
         { type: "action", action: "voiceRecognition", icon: "podcast.gif" }
     ]
