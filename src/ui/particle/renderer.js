237562d97bbdf8943ceff62e72432df6a439201d
hoangtu92 <hoangtu92@live.com>
2026-04-26 11:47:33 +0700

    update animatte

--- src/ui/particle/renderer.js
+++ src/ui/particle/renderer.js
@@ -83,15 +83,15 @@ export function animate(ctx) {
 
             u.uMouse.value.set(mouse.x, mouse.y);
 
-            u.uTurbulence.value = s.turbulence/10 + level * 2;
+            u.uTurbulence.value = s.turbulence/100 + level/10;
             u.uScatter.value = s.scatterSize + level * 3;
-            u.uBrightness.value = s.brightness/10 + level*10;
+            u.uBrightness.value = s.brightness/20 + level*10;
 
             if (u.uRipple) {
-                u.uRippleSpeed.value = s.rippleSpeed/10 + level * 2;
+                u.uRippleSpeed.value = s.rippleSpeed/100 + level;
             }
 
-            u.uCollision.value = s.collisionStrength/10 + level * 2;
+            u.uCollision.value = s.collisionStrength/100 + level/5;
         }
     }
 
